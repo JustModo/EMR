c9d0eaba26f07c1d0f77fb88021e7cfeaf42fb85
Yash <yashlaxman@gmail.com>
2024-05-25 01:12:55 +0530

    Update Home UI

--- EMRDoc/src/pages/Home.jsx
+++ EMRDoc/src/pages/Home.jsx
@@ -1,57 +1,37 @@
 import React from "react";
-import "../styles/home.css";
-import green from "@assets/blue3.png";
-import logo from "@assets/IDMLOGODARK.png";
-import { Input } from "@mui/material";
-import { SearchRounded } from "@mui/icons-material";
+import bgimage from "../assets/doc.png";
+import bgimage1 from "../assets/bg2.png";
+import { Button } from "@mui/material";
 
 export default function Home() {
   return (
-    <div
-      className="mycontainer bg-cover bg-center flex-col items-center justify-between"
-      style={{ backgroundImage: `url(${green})` }}
-    >
-      <div className="search justify-center items-center flex w-full h-full backdrop-blur-sm">
-        <Input
-          type="text"
-          name="search"
-          id="search"
-          placeholder="Search here"
-          className="w-8/12 h-12 bg-white rounded-xl p-5 border-gray-300 border-2"
-        />
-        <SearchRounded
-          sx={{ fontSize: 30 }}
-          className="text-black "
-        ></SearchRounded>
+    <div className="mycontainer flex-row">
+      <div
+        className="content h-full p-10 w-1/2 shadow-2xl flex flex-col z-10 justify-center gap-5 bg-center bg-no-repeat bg-cover"
+        style={{ backgroundImage: `url(${bgimage1})` }}
+      >
+        <h1 className="text-5xl font-bold text-left bg-center bg-no-repeat bg-cover text-sky-950 drop-shadow-lg font-noto-sans">
+          E-Life
+        </h1>
+        <h4 className="text-lg text-sky-950 font-bold font-noto-sans">
+          Welcome to E-Life , your comprehensive solution for managing
+          electronic medical records efficiently and securely.
+        </h4>
+        <div className="w-72">
+          <Button
+            variant="contained"
+            size="medium"
+            style={{ backgroundColor: "rgb(93, 101, 215)", color: "#fff" }}
+          >
+            See more
+          </Button>
+        </div>
       </div>
-      <div className="content hide-scrollbar">
-        <div className="left">
-          <h4>
-            Welcome to E-Life , your comprehensive solution for managing
-            electronic medical records efficiently and securely. Our app is
-            designed to streamline healthcare processes, improve patient care,
-            and enhance data security.
-          </h4>
-        </div>
-
-        <div className="main-content">
-          <h4>
-            Our EMR app allows healthcare providers to efficiently manage
-            patient records, appointments, and medical history all in one place.
-            With just a few clicks, you can access a patient’s full medical
-            history, schedule and track appointments, and ensure continuity of
-            care.
-          </h4>
-        </div>
-
-        <div className="right">
-          <h4>
-            We understand that ease of use is crucial. Our app features a
-            user-friendly interface designed to make navigation and data entry
-            as straightforward as possible, allowing healthcare providers to
-            focus on what they do best—caring for patients.
-          </h4>
-        </div>
+      <div
+        className="flex h-full w-1/2 bg-black bg-center bg-no-repeat bg-cover overflow-hidden"
+        style={{ backgroundImage: `url(${bgimage})` }}
+      >
+        {/* Content goes here */}
       </div>
     </div>
   );
